9c7c6cb49a8b85ca786d8df25bf05d41397220fa
Gonzalo Yustos <mgy.yustos@gmail.com>
2023-01-30 00:02:14 -0300

    commit 1

---
+++ src/test/java/com/portfolio/mgy/MgyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.mds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
